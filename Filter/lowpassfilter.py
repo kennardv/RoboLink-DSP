ee930e6e92d25d83730ed5e671f9a41dd1deb69f
Kennard Vermeiren <kennardvermeiren@gmail.com>
2015-11-25 19:18:15 +0000

    Lowpassfilter refactor

--- Filter/lowpassfilter.py
+++ Filter/lowpassfilter.py
@@ -42,17 +42,17 @@ class LowPassFilter:
             plt.plot(t, element[0], element[1], label=element[2])
             
         plt.xlabel('Time [sec]')
+        plt.xlim([0, 0.2])
         plt.grid()
         plt.legend()
         plt.subplots_adjust(hspace=0.35)
         plt.show()
         
 
-
 # Filter requirements.
 order = 6
-fs = 30.0       # sample rate, Hz
-cutoff = 3.667  # desired cutoff frequency of the filter, Hz
+fs = 30000.0       # sample rate, Hz
+cutoff = 200  # desired cutoff frequency of the filter, Hz
 
 lpf = LowPassFilter(order, fs, cutoff)
 
@@ -63,15 +63,21 @@ lpf.plot_freq_resp(b, a)
 
 # Demonstrate the use of the filter.
 # First make some data to be filtered.
-T = 5.0         # seconds
+T = 5.0               # seconds
 n = int(T * fs) # total number of samples
 t = np.linspace(0, T, n, endpoint=False)
-# "Noisy" data.  We want to recover the 1.2 Hz signal from this.
-data = np.sin(1.2*2*np.pi*t) + 1.5*np.cos(9*2*np.pi*t) + 0.5*np.sin(12.0*2*np.pi*t)
+# "Noisy" data.  We want to recover the 2 Hz signal from this.
+f1 = 200
+f2 = 1000
+sin1 = np.sin(2*np.pi*f1*t)
+sin2 = np.sin(2*np.pi*f2*t)
+data = sin1 + sin2
+
 
 # Filter the data
 y = lpf.butter_lowpass_filter(data)
 
 # Plot both the original and filtered signals.
 # Data, color, description
-lpf.plot([[data, 'b-', 'data'], [y, 'g-', 'filtered data']])
+lpf.plot([[y, 'g-', 'filtered signal'], [sin1, 'r-', 'sin1']])
+
